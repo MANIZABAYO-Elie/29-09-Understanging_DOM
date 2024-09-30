899e0d03714c3358e8a1d88d1e21679b95cda962
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-09-30 11:32:36 +0200

    file committed

--- script.js
+++ script.js
@@ -6,4 +6,6 @@
 // })
 
 const form = document.getElementById('form');
-const username = document.getElementById('username')
+const username = document.getElementById('username');
+const email  = document.getElementById('email');
+const password = document.getElementById('password');
